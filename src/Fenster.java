fbd27c9a63afbaf0c948cb2a0adc71241c8d3f25
IWillMakeTheSacrifice <123065532+IWillMakeTheSacrifice@users.noreply.github.com>
2023-01-21 17:27:18 +0100

    Better Variable Names

--- src/Fenster.java
+++ src/Fenster.java
@@ -5,12 +5,12 @@ import java.util.*;
 
 public class Fenster extends JFrame {
 
-	private JLabel jLabel1 = new JLabel();
+	private JLabel currentType = new JLabel();
 	private JButton backButton = new JButton();
-	private JButton jButton1 = new JButton();
-	private JButton jButton2 = new JButton();
-	private JButton jButton3 = new JButton();
-	private JButton jButton4 = new JButton();
+	private JButton effectiveButton = new JButton();
+	private JButton notEffectiveButton = new JButton();
+	private JButton immuneButton = new JButton();
+	private JButton RandomButton = new JButton();
 	private ArrayList<JButton> Typesbuttons = new ArrayList<JButton>();
 	private ArrayList<String> compareTypes = new ArrayList<String>();
 	Random rand = new Random();
@@ -32,11 +32,12 @@ public class Fenster extends JFrame {
 		Container cp = getContentPane();
 		cp.setLayout(null);
 
-		jLabel1.setBounds(150, 140, 350, 50);
-		cp.add(jLabel1);
+		currentType.setBounds(150, 140, 350, 50);
+		cp.add(currentType);
+
 		Datenbank.getTypes("SELECT Types FROM Pokemon_Types", 0);
 		int randType = rand.nextInt(Datenbank.Types.size());
-		jLabel1.setText(Datenbank.getTypes(randType, 0));
+		currentType.setText(Datenbank.getTypes(randType, 0));
 		backButton.setBounds(0, 0, 100, 50);
 		backButton.setText("Back");
 		backButton.setMargin(new Insets(2, 2, 2, 2));
@@ -46,47 +47,53 @@ public class Fenster extends JFrame {
 			}
 		});
 		cp.add(backButton);
-		jButton1.setBounds(150, 200, 100, 50);
-		jButton1.setText("Effective");
-		jButton1.setMargin(new Insets(2, 2, 2, 2));
-		jButton1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-				jButton1_ActionPerformed(evt);
-			}
-		});
-		cp.add(jButton1);
-		jButton2.setBounds(275, 200, 100, 50);
-		jButton2.setText("Not Effective");
-		jButton2.setMargin(new Insets(2, 2, 2, 2));
-		jButton2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-				jButton2_ActionPerformed(evt);
-			}
-		});
-		cp.add(jButton2);
-		jButton3.setBounds(400, 200, 100, 50);
-		jButton3.setText("Immune");
-		jButton3.setMargin(new Insets(2, 2, 2, 2));
-		jButton3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-				jButton3_ActionPerformed(evt);
-			}
-		});
-		cp.add(jButton3);
-		jButton4.setBounds(275, 275, 100, 50);
-		jButton4.setText("Random");
-		jButton4.setMargin(new Insets(2, 2, 2, 2));
-		jButton4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-				jButton4_ActionPerformed(evt);
-			}
-		});
-		cp.add(jButton4);
-		int count = 0;
-		for (int i = 0; i < 18; i++) {
 
+		effectiveButton.setBounds(150, 200, 100, 50);
+		effectiveButton.setText("Effective");
+		effectiveButton.setMargin(new Insets(2, 2, 2, 2));
+		effectiveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				effectiveButton_ActionPerformed(evt);
+			}
+		});
+		cp.add(effectiveButton);
+
+		notEffectiveButton.setBounds(275, 200, 100, 50);
+		notEffectiveButton.setText("Not Effective");
+		notEffectiveButton.setMargin(new Insets(2, 2, 2, 2));
+		notEffectiveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				notEffectiveButton_ActionPerformed(evt);
+			}
+		});
+		cp.add(notEffectiveButton);
+
+		immuneButton.setBounds(400, 200, 100, 50);
+		immuneButton.setText("Immune");
+		immuneButton.setMargin(new Insets(2, 2, 2, 2));
+		immuneButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				immuneButton_ActionPerformed(evt);
+			}
+		});
+		cp.add(immuneButton);
+
+		RandomButton.setBounds(275, 275, 100, 50);
+		RandomButton.setText("Random");
+		RandomButton.setMargin(new Insets(2, 2, 2, 2));
+		RandomButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				RandomButton_ActionPerformed(evt);
+			}
+		});
+		cp.add(RandomButton);
+
+		int count = 0;
+		
+		for (int i = 0; i < 18; i++) {
 			Typesbuttons.add(new JButton());
 		}
+		// Typen Ersteller der Buttons
 		Datenbank.getTypes("SELECT Types FROM Pokemon_Types", 0);
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 6; j++) {
@@ -100,6 +107,7 @@ public class Fenster extends JFrame {
 					}
 				});
 				cp.add(Typesbuttons.get(count));
+				
 				Typesbuttons.get(count).setVisible(false);
 				count++;
 			}
@@ -109,9 +117,9 @@ public class Fenster extends JFrame {
 	} // end of public Fenster
 
 	// Anfang Methoden
-	public void jButton1_ActionPerformed(ActionEvent evt) {
-		String Type = jLabel1.getText();
-		if (jButton1.getText() == "Effective") {
+	public void effectiveButton_ActionPerformed(ActionEvent evt) {
+		String Type = currentType.getText();
+		if (effectiveButton.getText() == "Effective") {
 			Datenbank.getTypes("SELECT Effective FROM Pokemon_Type_Effective WHERE Type LIKE '" + Type + "'", 1);
 			int size = Datenbank.effective_against_Types.size();
 			compareTypes.clear();
@@ -119,18 +127,18 @@ public class Fenster extends JFrame {
 //				System.out.println(i + " " + Datenbank.getTypes(i, 1));
 				compareTypes.add(Datenbank.getTypes(i, 1));
 			}
-			jButton2.setVisible(false);
-			jButton3.setVisible(false);
-			jButton4.setVisible(false);
+			notEffectiveButton.setVisible(false);
+			immuneButton.setVisible(false);
+			RandomButton.setVisible(false);
 			for (int i = 0; i < 18; i++) {
 				Typesbuttons.get(i).setVisible(true);
 			}
 		}
 	} // end of jButton1_ActionPerformed
 
-	public void jButton2_ActionPerformed(ActionEvent evt) {
-		String Type = jLabel1.getText();
-		if (jButton2.getText() == "Not Effective") {
+	public void notEffectiveButton_ActionPerformed(ActionEvent evt) {
+		String Type = currentType.getText();
+		if (notEffectiveButton.getText() == "Not Effective") {
 			Datenbank.getTypes("SELECT Not_Effective FROM Pokemon_Type_Not_Effective WHERE Type LIKE '" + Type + "'",
 					2);
 			int size = Datenbank.not_effective_against_Types.size();
@@ -140,63 +148,64 @@ public class Fenster extends JFrame {
 				compareTypes.add(Datenbank.getTypes(i, 2));
 			}
 //			System.out.println(compareTypes);
-			jButton1.setVisible(false);
-			jButton3.setVisible(false);
-			jButton4.setVisible(false);
+			effectiveButton.setVisible(false);
+			immuneButton.setVisible(false);
+			RandomButton.setVisible(false);
 			for (int i = 0; i < 18; i++) {
 				Typesbuttons.get(i).setVisible(true);
 			}
 		}
 	}
 
-	public void jButton3_ActionPerformed(ActionEvent evt) {
-		String Type = jLabel1.getText();
-		if (jButton3.getText() == "Immune") {
+	public void immuneButton_ActionPerformed(ActionEvent evt) {
+		String Type = currentType.getText();
+		if (immuneButton.getText() == "Immune") {
 			Datenbank.getTypes("SELECT Immune FROM Pokemon_Type_Immune WHERE Type LIKE '" + Type + "'", 3);
 			int size = Datenbank.immune_Types.size();
 			compareTypes.clear();
 			for (int i = 0; i < size; i++) {
 //				System.out.println(i + " " + Datenbank.getTypes(i, 3));
 				compareTypes.add(Datenbank.getTypes(i, 3));
-				
+
 			}
-			jButton1.setVisible(false);
-			jButton2.setVisible(false);
-			jButton4.setVisible(false);
+			effectiveButton.setVisible(false);
+			notEffectiveButton.setVisible(false);
+			RandomButton.setVisible(false);
 			for (int i = 0; i < 18; i++) {
 				Typesbuttons.get(i).setVisible(true);
 			}
 		}
 	}
 
-	public void jButton4_ActionPerformed(ActionEvent evt) {
+	public void RandomButton_ActionPerformed(ActionEvent evt) {
 		Datenbank.getTypes("SELECT Types FROM Pokemon_Types", 0);
 		int i = rand.nextInt(Datenbank.Types.size());
-		jLabel1.setText(Datenbank.getTypes(i, 0));
+		currentType.setText(Datenbank.getTypes(i, 0));
 	}
 
 	public void Typesbuttons_ActionPerformed(ActionEvent evt, String tempTypes) {
-		for(int i = 0; i < compareTypes.size();i++) {
-			if(tempTypes.equals(compareTypes.get(i))) {
+		for (int i = 0; i < compareTypes.size(); i++) {
+			
+			if (tempTypes.equals(compareTypes.get(i))) {
+				// Hier muss gearbeitet werden
 				System.out.println("Right");
 				countRightTypes++;
-				
-				
+
 			}
 		}
-		if(countRightTypes == compareTypes.size()) {
+		if (countRightTypes == compareTypes.size()) {
 			System.out.println("That was all");
 			countRightTypes = 0;
 		}
-		
-		
+
 	}
+
 	public void backButton_ActionPerformed(ActionEvent evt) {
-		jButton1.setVisible(true);
-		jButton2.setVisible(true);
-		jButton3.setVisible(true);
-		jButton4.setVisible(true);
-		for(int i = 0; i < 18; i++) {
+		effectiveButton.setVisible(true);
+		notEffectiveButton.setVisible(true);
+		immuneButton.setVisible(true);
+		RandomButton.setVisible(true);
+		for (int i = 0; i < 18; i++) {
 			Typesbuttons.get(i).setVisible(false);
 		}
 	}
